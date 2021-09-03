707ed5028b5e4544e4cfda83a27eb5b4b63ea169
Greg Doud <gpdoud@gmail.com>
2021-09-03 14:20:22 -0400

    Add project files.

---
+++ TestLargeTableDataLib/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLargeTableDataLib.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<Number> Numbers {  get; set; }
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+	}
+}
---
+++ TestLargeTableDataLib/Models/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLargeTableDataLib.Models
+{
+	public class Number
+	{
+		public int Id {  get; set; }
+		public int Nbr{ get; set; }
+	}
+}
